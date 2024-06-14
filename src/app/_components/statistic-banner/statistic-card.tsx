e5b72c2abf955bbd90f742704ebcb55dc94bb3e5
3N14C <3n14c@vk.com>
2024-06-14 20:33:00 +0800

    updated

--- src/app/_components/statistic-banner/statistic-card.tsx
+++ src/app/_components/statistic-banner/statistic-card.tsx
@@ -16,35 +16,32 @@ interface IProps {
 const mark = Marck_Script({ subsets: ["cyrillic"], weight: ["400"] });
 
 export const StatisticCard: FC<IProps> = ({ number, img, name }) => {
-  const [ref, inView] = useInView();
 
   return (
-    <animated.div ref={ref} className="">
-      {inView && (
-        <CountUp end={number} duration={5}>
-          {({ countUpRef }) => (
-            <div className="flex flex-col gap-3 items-center">
-              <div className={cn(mark.className, "flex items-center")}>
-                <span
-                  className={cn("text-6xl font-bold text-[#6f4e37]")}
-                  ref={countUpRef}
-                />
-                <p className="text-2xl">/{name}</p>
-              </div>
-
-              <div className="h-[1px] w-1/2 bg-gray-400" />
-
-              <Image
-                src={img}
-                alt={name}
-                width={1000}
-                height={1000}
-                className="w-40 h-40"
+    <animated.div className="">
+      <CountUp end={number} duration={5}>
+        {({ countUpRef }) => (
+          <div className="flex flex-col gap-3 items-center">
+            <div className={cn(mark.className, "flex items-center")}>
+              <span
+                className={cn("text-6xl font-bold text-[#6f4e37]")}
+                ref={countUpRef}
               />
+              <p className="text-2xl">/{name}</p>
             </div>
-          )}
-        </CountUp>
-      )}
+
+            <div className="h-[1px] w-1/2 bg-gray-400" />
+
+            <Image
+              src={img}
+              alt={name}
+              width={1000}
+              height={1000}
+              className="w-40 h-40"
+            />
+          </div>
+        )}
+      </CountUp>
     </animated.div>
   );
 };
