2e518d6aee8bfe03edf47d8b37cfcffce89da3a1
3N14C <3n14c@vk.com>
2024-06-14 20:35:56 +0800

    fds

--- src/app/_components/statistic-banner/statistic-banner.tsx
+++ src/app/_components/statistic-banner/statistic-banner.tsx
@@ -2,6 +2,12 @@
 
 import { FC, useEffect, useState } from "react";
 import { StatisticCard } from "./statistic-card";
+import CountUp from "react-countup";
+import { cn } from "@/lib/utils";
+import { Marck_Script } from "next/font/google";
+import Image from "next/image";
+
+const mark = Marck_Script({ subsets: ["cyrillic"], weight: ["400"] });
 
 export const StatisticBanner: FC = () => {
   const [mounted, setMounted] = useState(false);
@@ -14,7 +20,7 @@ export const StatisticBanner: FC = () => {
 
   return (
     <div className="grid lg:grid-cols-4 items-center lg:gap-0">
-      <StatisticCard name="блюд" number={103} img="/img/dishes-png.png" />
+      {/* <StatisticCard name="блюд" number={103} img="/img/dishes-png.png" />
       <StatisticCard
         name="посетителей"
         number={2389}
@@ -25,7 +31,31 @@ export const StatisticBanner: FC = () => {
         name="часов работы"
         number={2589}
         img="/img/dishes-png.png"
-      />
+      /> */}
+
+      <CountUp end={2389} duration={5}>
+        {({ countUpRef }) => (
+          <div className="flex flex-col gap-3 items-center">
+            <div className={cn(mark.className, "flex items-center")}>
+              <span
+                className={cn("text-6xl font-bold text-[#6f4e37]")}
+                ref={countUpRef}
+              />
+              <p className="text-2xl">fdsf</p>
+            </div>
+
+            <div className="h-[1px] w-1/2 bg-gray-400" />
+
+            <Image
+              src={"/img/dishes-png.png"}
+              alt={"fds"}
+              width={1000}
+              height={1000}
+              className="w-40 h-40"
+            />
+          </div>
+        )}
+      </CountUp>
     </div>
   );
 };
